fc76e2298252f39cbc601036b9992117ee78ba9e
ParaschivGeorge <georgegmtt@gmail.com>
2019-05-26 17:03:29 +0300

    first commit

---
+++ src/test/java/com/paraett/springcloudconfigurationserver/SpringCloudConfigurationServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paraett.springcloudconfigurationserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigurationServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
